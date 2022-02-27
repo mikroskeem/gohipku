a83d8cc923c2195a905a4c5c42f14adc2db79dde
jtagcat <git-12dbd862@jtag.cat>
2022-02-28 00:47:08 +0200

    uncommet portability tests

--- pkg/encode_test.go
+++ pkg/encode_test.go
@@ -7,18 +7,6 @@ import (
 	"inet.af/netaddr"
 )
 
-var ipv4Set = map[string]string{
-	"0.0.0.0":         "The agile beige ape\naches in the ancient canyon.\nAutumn colors blow.\n",
-	"127.0.0.1":       "The hungry white ape\naches in the ancient canyon.\nAutumn colors crunch.\n",
-	"82.158.98.2":     "The fearful blue newt\nwakes in the foggy desert.\nAutumn colors dance.\n",
-	"255.255.255.255": "The weary white wolf\nyawns in the wind-swept wetlands.\nYellowwood leaves twist.\n",
-}
-var ipv6Set = map[string]string{
-	"0:0:0:0:0:0:0:0":                         "Ace ants and ace ants\naid ace ace ace ace ace ants.\nAce ants aid ace ants.\n",
-	"2c8f:27aa:61fd:56ec:7ebe:d03a:1f50:475f": "Cursed mobs and crazed queens\nfeel wrong gruff tired moist slow sprats.\nFaint bulls dread fond fruits.\n",
-	"ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff": "Young yaks and young yaks\ntend young young young young young yaks.\nYoung yaks tend young yaks.\n",
-}
-
 func TestIPv4(t *testing.T) {
 	ip, err := netaddr.ParseIP("193.4.5.6") // lo: 0xffff00000000..0xffffffffffff
 	assert.NotNil(t, ip)
---
+++ pkg/gohipku_test.go
@@ -0,0 +1,42 @@
+package gohipku
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var ipv4Set = map[string]string{
+	"0.0.0.0":         "The agile beige ape\naches in the ancient canyon.\nAutumn colors blow.\n",
+	"127.0.0.1":       "The hungry white ape\naches in the ancient canyon.\nAutumn colors crunch.\n",
+	"82.158.98.2":     "The fearful blue newt\nwakes in the foggy desert.\nAutumn colors dance.\n",
+	"255.255.255.255": "The weary white wolf\nyawns in the wind-swept wetlands.\nYellowwood leaves twist.\n",
+}
+var ipv6Set = map[string]string{
+	"0:0:0:0:0:0:0:0":                         "Ace ants and ace ants\naid ace ace ace ace ace ants.\nAce ants aid ace ants.\n",
+	"2c8f:27aa:61fd:56ec:7ebe:d03a:1f50:475f": "Cursed mobs and crazed queens\nfeel wrong gruff tired moist slow sprats.\nFaint bulls dread fond fruits.\n",
+	"ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff": "Young yaks and young yaks\ntend young young young young young yaks.\nYoung yaks tend young yaks.\n",
+}
+
+func TestPorabilityIPv4(t *testing.T) {
+	testPortability(t, ipv4Set)
+}
+func TestPorabilityIPv6(t *testing.T) {
+	testPortability(t, ipv6Set)
+}
+
+func testPortability(t *testing.T, ip_hipku map[string]string) {
+	for ip, hipku := range ip_hipku {
+		ip := net.ParseIP(ip)
+		assert.NotNil(t, ip)
+
+		h, ok := Encode(ip)
+		assert.True(t, ok)
+		assert.Equal(t, hipku, h)
+
+		ip2, err := Decode(hipku)
+		assert.Nil(t, err)
+		assert.Equal(t, ip, ip2)
+	}
+}
