f35e4bcd85feaef4f9cb54318f6fa86f42d695bd
jtagcat <git-12dbd862@jtag.cat>
2022-02-28 00:44:38 +0200

    decode err tests

--- pkg/decode_test.go
+++ pkg/decode_test.go
@@ -22,3 +22,28 @@ func TestHipku6(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, net.ParseIP("67d1:8f17:b5a9:dbee:89d4:42a6:7154:4f69"), ip)
 }
+
+func TestFakePoems(t *testing.T) {
+	_, err := Decode("The silent black ape\n") // not enough words
+	assert.ErrorIs(t, err, ErrInvalidAddr)
+
+	_, err = Decode("The silent black ape\n" +
+		"eats in the turtle grasslands.\n" + // turtle
+		"Autumn colors grow.\n")
+	assert.ErrorIs(t, err, ErrInvalidAddr)
+
+	_, err = Decode("The silent black ape\n" +
+		"eats in the ancient grasslands.\n" +
+		"Autumn colors grow tall.\n") // tall, extra
+	assert.ErrorIs(t, err, ErrInvalidAddr)
+
+	_, err = Decode("The silent black ape\n" +
+		"eats in the ancient grasslands.\n" +
+		"Autumn cats hell and back and grow.\n") // cats hell and back and
+	assert.ErrorIs(t, err, ErrInvalidAddr)
+
+	_, err = Decode("The silent black ape\n" +
+		"eats in the ancient grasslands.\n" +
+		"Autumn") // cut short
+	assert.ErrorIs(t, err, ErrInvalidAddr)
+}
